b66922dd59c954110b4b6fe7c653c90a9c9f2071
Manish Pandit <manish.pandit@autodesk.com>
2025-07-22 06:29:38 -0700

    Initial Commit

---
+++ NightlyFortune/NightlyFortuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightlyFortuneApp.swift
+//  NightlyFortune
+//
+//  Created by Manish Pandit on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct NightlyFortuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
